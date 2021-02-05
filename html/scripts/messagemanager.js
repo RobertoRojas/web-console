bf1349b5987dfffcc0ec04bd4bdf7c521b217031
RobertoRojas <jroberto_rojasv@hotmail.com>
2021-02-05 11:38:32 -0600

    Add breakline and show title in blog-read

--- html/scripts/messagemanager.js
+++ html/scripts/messagemanager.js
@@ -1,3 +1,6 @@
+function showLineBreak() {
+    document.getElementById("shell").innerHTML = document.getElementById("shell").innerHTML + "<div><span style='black_font'>&nbsp;</span></div>";
+}
 function showImage(image) {
     document.getElementById("shell").innerHTML = document.getElementById("shell").innerHTML + "<div class='image'><div class='image_picture' style='background-image: url(\"" + image.value + "\");'></div><div class='image_filter'><span>[" + image.identifier + "]</span></div></div>";
 }
