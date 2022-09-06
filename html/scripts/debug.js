cdd2cbcb5e00d85d261e57a1e3e2cef6c5678992
Roberto Rojas <developer@robertorojas.com.mx>
2022-09-05 22:17:36 -0500

    Add debug params

--- html/scripts/debug.js
+++ html/scripts/debug.js
@@ -1,3 +1,4 @@
-function debug() {
-    showMessage("<span class='yellow_font'>This is the debug function</span>");
+function debug(arguments) {
+    arguments = arguments.join("").trim();
+    showMessage("<span class='yellow_font red_back'>Arguments: " + arguments + "</span>");
 }
