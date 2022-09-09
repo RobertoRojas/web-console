260ff18c6aeb2fed2a02aa0f8943777c1af5699b
Roberto Rojas <developer@robertorojas.com.mx>
2022-09-08 22:14:04 -0500

    Update site page

--- scripts/configuration.js
+++ scripts/configuration.js
@@ -1,10 +1,10 @@
 // General configurations
 
-var site = "http://" + window.location.hostname;
-var version = "1.1.0";
-var shellName = "r2-shell";
+var site = 'https://robertorojas.github.io/web-console/';
+var version = '1.1.0';
+var shellName = 'r2-shell';
 
 // Blog configuration
-var blogLang = "en";
-var blogOwner = "RobertoRojas";
-var blogRepo = "blog";
+var blogLang = 'en';
+var blogOwner = 'RobertoRojas';
+var blogRepo = 'blog';
